10daa2a3f442d04feeec00835dcb50097d8f3b16
Abhay <abhayrajsinghmandloi@gmail.com>
2026-02-25 13:03:08 +0530

    OOPs-1 Practice

---
+++ OOPS/practice.py
@@ -0,0 +1,25 @@
+class Bank:
+    def __init__(self,amount,account):
+        self.amount = amount
+        self.account = account
+
+    def credit(self,amount):
+        self.amount += amount
+        print(f"Credited {amount}. New balance: {self.amount}")
+
+    def debit(self,amount):
+        if amount > self.amount:
+            print("Insufficient funds")
+        else:
+            self.amount -= amount
+            print(f"Debited {amount}. New balance: {self.amount}")
+    
+    def balance(self):
+        print(f"Current balance: {self.amount}")
+
+
+
+b1 = Bank(1000,"123456789")
+b1.credit(500)
+b1.debit(200)
+b1.balance()
